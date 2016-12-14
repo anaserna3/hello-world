dd27412d6ce2ae2e86072c7865dca4dea049ff5d
anaserna3 <anaserna3@gmail.com>
2016-12-14 10:21:01 -0600

    Template added

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * LLamada a la Base de Datos
+ */
+
+////BASE DE DATOS LOCAL COMPUTADORA ANA
+define("DB_HOST", "localhost"); //Nombre del host
+define("DB_USER", "root"); //User name de la Base de Datos
+define("DB_PASSWORD", "Alst5025"); //Passwor de la Base de Datos
+define("DB_DATABASE", "incom"); //Nombre de la base de Datos
+
+?>
